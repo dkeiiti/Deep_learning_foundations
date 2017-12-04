e0355b5c686d4b4604c73944c74249f75387a735
dkeiiti <30915922+dkeiiti@users.noreply.github.com>
2017-12-04 19:09:07 -0300

    Update my_answers.py

--- my_answers.py
+++ my_answers.py
@@ -139,7 +139,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 10000000
+iterations = 10000
 learning_rate = 0.1
-hidden_nodes = 2
+hidden_nodes = 88
 output_nodes = 1
